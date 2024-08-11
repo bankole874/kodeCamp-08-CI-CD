201e3c3c9e22524e15095fff223f1c66300eb6cb
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2024-08-11 23:16:30 +0100

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    message = os.getenv('WELCOME_MESSAGE', "Hello, Welcome to KodeCamp DevOps Bootcamp!")
+    return message
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
